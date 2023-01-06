a4db9745f97d5290e15b0ac5a46d3edd5e213140
sammy2997 <samueladama@ymail.com>
2023-01-06 09:53:43 -0600

    Initial Commit

---
+++ LululemonTakeHomeProject/LululemonTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LululemonTakeHomeProjectApp.swift
+//  LululemonTakeHomeProject
+//
+//  Created by Samuel Adama on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LululemonTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
